1cbd052623ac90d94d1873f70c11b04428096bc6
QC.L <liqichang@lanou3g.com>
2017-06-02 11:25:01 +0800

    添加轮播图标题

--- src/components/cycle-image/cycle-image.vue
+++ src/components/cycle-image/cycle-image.vue
@@ -2,6 +2,8 @@
   <div class="cycle-image">
     <mt-swipe style="width: 100%; height: 100%">
       <mt-swipe-item v-for="(top, index) in top_stories" :style="{background: 'url(' + top.image + ') no-repeat center center', 'background-size': 'cover'}" :key="index">
+        <div class="mask"></div>
+        <h1>{{top.title}}</h1>
       </mt-swipe-item>
     </mt-swipe>
   </div>
@@ -25,4 +27,15 @@
     width: 100%
     height: auto
     max-height: 100%
+  .mask
+    width: 100%
+    height: 100%
+    background: rgba(0, 0, 0, 0.4)
+  h1
+    color: $color-title
+    bottom: 30px
+    position: absolute
+    width: 100%
+    height: auto
+    font-size: $font-size-title
 </style>
